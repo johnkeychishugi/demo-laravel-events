0c1edc42effab32c8863513fc13fe2688fe4d43a
John Chishugi <jkchishugi@gmail.com>
2017-11-23 22:42:06 +0200

    UpdateHome page

--- resources/views/events/home.blade.php
+++ resources/views/events/home.blade.php
@@ -10,5 +10,7 @@
 	<h2>Test Git Repository</h2>
 	<h2>Test Git Repository</h2>
 	<h2>Test Git Repository.</h2>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ipsam assumenda incidunt deserunt pariatur voluptas possimus asperiores obcaecati quis optio ad necessitatibus debitis accusantium, cum delectus earum dignissimos, mollitia eligendi.</p>
 </body>
 </html>
